4b5aa8c8e26275574de4650bdf73bac1255fae1f
LimeVista <LimeVista@outlook.com>
2026-01-08 22:45:33 +0800

    refactor(example): 重构示例应用界面和功能
    - 移除文本编辑器预览功能，简化界面布局
    - 添加SelectionArea组件支持文本选择
    - 实现AutoRichText事件回调处理机制
    - 添加事件弹窗显示功能
    - 更新示例文本内容和格式
    - 修复字体资源配置错误
    - 更新README文档内容
    - 移除未使用的控制器和相关代码

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# auto_rich_text_example
+# Auto Rich Text for Flutter Example
 
-Auto Rich Text for Flutter example.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+* See https://github.com/LimeVista/auto_rich_text
--- lib/main.dart
+++ lib/main.dart
@@ -37,65 +37,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   _HomePageState();
 
-  final TextEditingController _controller = TextEditingController(
-    text: """
-=== Text Styles ===
-<bold>Bold Text</bold>
-<italic>Italic Text</italic>
-<color=#4C5A79>Colored Text</color>
-<color=lime>Named Color</color>
-<size=20>Large Text</size>
-<size=+5>Increased Size</size>
-<size=-3>Decreased Size</size>
-<weight=w900>Extra Bold</weight>
-<weight=w300>Light Weight</weight>
-<font=sans>Sans Font</font>
-
-=== Decoration Tags ===
-<delete=#FF0000>Red Strikethrough</delete>
-<delete='color:blue; style:dashed; thickness:2.0'>Custom Strikethrough</delete>
-<u=#FF0000>Red Underline</u>
-<u='color:orange; style:wavy; thickness:1.5'>Wavy Underline</u>
-
-=== Gradient Tags ===
-<gradient='colors:red,yellow'>Linear Gradient</gradient>
-<gradient='colors:blue,purple,pink; type:linear'>Multi-color Gradient</gradient>
-<gradient='colors:cyan,lime; type:radial; radius:0.8'>Radial Gradient</gradient>
-
-=== Alignment Tags ===
-<align=top>Top Aligned</align>
-<align=middle>Middle Aligned</align>
-<align=bottom>Bottom Aligned</align>
-
-=== Interactive Tags ===
-<click='id:link1'>Clickable Text</click>
-<click='id:link2; type:double'>Double Click</click>
-<click='id:link3; type:long; args:param1,param2'>Long Press with Args</click>
-<tap='id:tap1; types:tap'>Tap Text</tap>
-<tap='id:tap2; types:tapDown,tapUp'>Multi-type Touch</tap>
-
-=== Placeholder Tags ===
-<icon='code:66083; font-family:IconFont'/>
-<image='file:ic_music.png; width:24; height:24'/>
-<ref=custom/>
-
-=== Combined Styles ===
-<bold><italic><color=#FF5722>Bold Italic Orange</color></italic></bold>
-<size=18><weight=w600><u=blue>Large Bold Blue Underline</u></weight></size>
-<gradient='colors:gold,orange'><bold>Gradient Bold</bold></gradient>
-""",
-  );
-
   final TextStyle _textStyle = const TextStyle(
     fontSize: 14,
     color: Colors.black,
   );
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -107,44 +52,22 @@ class _HomePageState extends State<HomePage> {
         centerTitle: false,
       ),
       extendBody: true,
-      body: Row(
-        children: [
-          Expanded(child: _buildPreview()),
-          Container(width: 1, color: Colors.grey),
-          Expanded(child: _buildEditor()),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildPreview() {
-    return SingleChildScrollView(
-      child: Container(
-        padding: EdgeInsets.all(12),
-        alignment: Alignment.topLeft,
-        child: AutoRichText(
-          text: _controller.text,
-          style: _textStyle,
-          escape: true,
-          onRefCallback: _onRef,
-          builder: (span, style) => Text.rich(span, style: style),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(12),
+          alignment: Alignment.topLeft,
+          child: SelectionArea(
+            child: AutoRichText(
+              text: _text,
+              style: _textStyle,
+              escape: true,
+              onRefCallback: _onRef,
+              onEventCallback: _onEvent,
+              builder: (span, style) => Text.rich(span, style: style),
+            ),
+          ),
         ),
       ),
-    );
-  }
-
-  Widget _buildEditor() {
-    return TextField(
-      controller: _controller,
-      expands: true,
-      maxLines: null,
-      textAlignVertical: TextAlignVertical.top,
-      decoration: InputDecoration(
-        border: InputBorder.none,
-        contentPadding: const EdgeInsets.all(12.0),
-        hintText: 'Enter some text...',
-      ),
-      style: _textStyle,
     );
   }
 
@@ -156,5 +79,84 @@ class _HomePageState extends State<HomePage> {
         color: Colors.blue,
       ),
     );
+  }
+
+  void _onEvent(AutoRichTextEvent evt) {
+    showDialog(
+      context: context,
+      builder: (context) => Center(
+        child: Material(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Text("Type: ${evt.type}\nId: ${evt.id}\nArgs: ${evt.args}"),
+          ),
+        ),
+      ),
+    );
+  }
+
+  String get _text {
+    return """
+<color=green># &lt;bold&gt;Bold Text&lt;/bold&gt;</color>
+<color=green># &lt;weight=w700&gt;Bold&lt;/weight&gt;</color>
+<bold>Bold Text</bold>
+<weight=w900>Extra Bold</weight>
+<weight=w300>Light Weight</weight>
+
+<color=green># &lt;italic&gt;Italic Text&lt;/italic&gt;</color>
+<italic>Italic Text</italic>
+
+<color=green># &lt;color=#4C5A79&gt;Colored Text&lt;/color&gt;</color>
+<color=green># &lt;color=orange&gt;Named Text&lt;/color&gt;</color>
+<color=#4C5A79>Colored Text</color>
+<color=orange>Named Color</color>
+
+<color=green># &lt;size=20&gt;Large Text&lt;/size&gt;</color>
+<color=green># &lt;size=+2&gt;Increased Size&lt;/size&gt;</color>
+<color=green># &lt;size=-2&gt;Decreased Size&lt;/size&gt;</color>
+<size=20>Large Text</size>
+<size=+2>Increased Size</size>
+<size=-2>Decreased Size</size>
+
+<color=green># &lt;color=#FFF&gt;&lt;gradient='colors:red,yellow'&gt;Linear Gradient&lt;/gradient&gt;&lt;/color&gt;</color>
+<color=green># &lt;color=#FFF&gt;&lt;gradient='colors:blue,purple,pink; type:linear'&gt;Multi-color Gradient&lt;/gradient&gt;&lt;/color&gt;</color>
+<color=green># &lt;color=#FFF&gt;&lt;gradient='colors:cyan,lime; type:radial; radius:0.8'&gt;Radial Gradient&lt;/gradient&gt;&lt;/color&gt;</color>
+<color=#FFF><gradient='colors:red,yellow'>Linear Gradient</gradient></color>
+<color=#FFF><gradient='colors:blue,purple,pink; type:linear'>Multi Color Gradient</gradient></color>
+<color=#FFF><gradient='colors:cyan,lime; type:radial; radius:0.8'>Radial Gradient</gradient></color>
+
+<color=green># &lt;icon='code:0xE800; font-family:Icon'/&gt;</color>
+<color=green># &lt;image='file:img_example.png; width:32; height:32'/&gt;</color>
+<icon='code:0xE800; font-family:Icon'/>
+<image='file:img_example.png; width:32; height:32'/>
+
+<color=green># &lt;font=Borel&gt;Borel Font&lt;/font&gt;</color>
+<font=Borel>Borel Font</font>
+
+<color=green># &lt;delete=#FF0000&gt;Red Strikethrough&lt;/delete&gt;</color>
+<color=green># &lt;delete='color:blue; style:dashed; thickness:2.0'&gt;Custom Strikethrough&lt;/delete&gt;</color>
+<color=green># &lt;u=#FF0000&gt;Red Underline&lt;/u&gt;</color>
+<color=green># &lt;u='color:orange; style:wavy; thickness:1.5'&gt;Wavy Underline&lt;/u&gt;</color>
+<delete=#FF0000>Red Strikethrough</delete>
+<delete='color:blue; style:dashed; thickness:2.0'>Custom Strikethrough</delete>
+<u=#FF0000>Red Underline</u>
+<u='color:orange; style:wavy; thickness:1.5'>Wavy Underline</u>
+
+<color=green># &lt;click='id:click'&gt;Clickable Text&lt;/click&gt;</color>
+<color=green># &lt;click='id:double; type:double'&gt;Double Click&lt;/click&gt;</color>
+<color=green># &lt;click='id:long; type:long; args:a,b'&gt;Long Press with Args&lt;/click&gt;</color>
+<color=green># &lt;tap='id:tap; types:tap'&gt;Tap Text&lt;/tap&gt;</color>
+<click='id:click'>Clickable Text</click>
+<click='id:double; type:double'>Double Click</click>
+<click='id:long; type:long; args:a,b'>Long Press with Args</click>
+<tap='id:tap; types:tap'>Tap Text</tap>
+
+<color=green># &lt;bold&gt;&lt;italic&gt;&lt;color=#FF5722&gt;Bold Italic Orange&lt;/color&gt;&lt;/italic&gt;&lt;/bold&gt;</color>
+<color=green># &lt;size=18&gt;&lt;weight=w600&gt;&lt;u=blue&gt;Large Bold Blue Underline&lt;/u&gt;&lt;/weight&gt;&lt;/size&gt;</color>
+<bold><italic><color=#FF5722>Bold Italic Orange</color></italic></bold>
+<size=18><weight=w600><u=blue>Large Bold Blue Underline</u></weight></size>
+""";
   }
 }
